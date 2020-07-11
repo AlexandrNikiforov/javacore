b7bf7443f9e08c2b581eeced8e81547af9c90929
AlexandrNikifrov <nikiforov.san.sanich@gmail.com>
2020-07-11 08:29:03 +0300

    Printed "This is the line for commit on 11.07.2020 at 08-30"

--- javacore/src/main/java/com/chapter03/Main.java
+++ javacore/src/main/java/com/chapter03/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println ("Hello");
+        System.out.println("This is the line for commit on 11.07.2020 at 08-30");
 
 
     }

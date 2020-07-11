785d3d0fb7dfb276ad987284008ec8c632a81039
AlexandrNikifrov <nikiforov.san.sanich@gmail.com>
2020-07-11 09:07:25 +0300

    Printed ".. at 09-00", added NewTestClass

--- javacore/src/main/java/com/chapter03/Main.java
+++ javacore/src/main/java/com/chapter03/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         System.out.println ("Hello");
         System.out.println("This is the line for commit on 11.07.2020 at 08-30");
-        System.out.println("This is the line for commit on GitHub 11.07.2020 at 08-50");
+        System.out.println("This is the line for commit on GitHub 11.07.2020 at 09-00");
 
 
     }
---
+++ javacore/src/main/java/com/chapter03/NewTestClass.java
@@ -0,0 +1,4 @@
+package main.java.com.chapter03;
+
+public class NewTestClass {
+}

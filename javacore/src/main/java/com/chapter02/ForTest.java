a770c0a32a1f0cd18ef4ec2efc3f837e45996bec
AlexandrNikifrov <nikiforov.san.sanich@gmail.com>
2020-07-11 23:41:45 +0300

    Added an example program from chapter 2

---
+++ javacore/src/main/java/com/chapter02/ForTest.java
@@ -0,0 +1,19 @@
+package main.java.com.chapter02;
+/* ForTest program from
+"JAVA. Complete Reference" chapter 02
+ */
+
+public class ForTest {
+
+    public static void main(String[] args) {
+        for (int x = 0; x < 10; x++) {
+            System.out.println("This is x: " + x);
+
+        }
+
+
+    }
+
+
+
+}

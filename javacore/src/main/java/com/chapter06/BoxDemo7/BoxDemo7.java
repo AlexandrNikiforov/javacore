362b679bedcd6a1adc33a74ca983ec5110e3a763
AlexandrNikifrov <nikiforov.san.sanich@gmail.com>
2020-07-24 19:48:42 +0300

    Added examples from the chapter 6

---
+++ javacore/src/main/java/com/chapter06/BoxDemo7/BoxDemo7.java
@@ -0,0 +1,17 @@
+package main.java.com.chapter06.BoxDemo7;
+
+public class BoxDemo7 {
+
+    public static void main(String[] args) {
+        Box myBox1 = new Box(10, 20, 15);
+        Box myBox2 = new Box(3, 6, 9);
+
+
+
+
+        System.out.println("площадь равна: " + myBox1.vol());
+        System.out.println("площадь равна: " + myBox2.vol());
+    }
+
+
+}
---
+++ javacore/src/main/java/com/chapter06/TheFirstPost.java
@@ -0,0 +1,11 @@
+package main.java.com.chapter06;
+
+public class TheFirstPost {
+
+    /*
+
+    -----WELCOME-----
+
+     */
+
+}
